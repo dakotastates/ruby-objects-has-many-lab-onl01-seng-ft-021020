b81b1bbda4c8c9841e2c271c1010b08385a19ff5
Dakota States <dakota.states@gmail.com>
2020-02-26 06:14:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
 
   attr_accessor :name, :songs
 
-  @@song_count = 0
+  @@song_count = 1
 
   def initialize(name)
     @name = name
@@ -17,14 +17,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    #@@song_count += 1
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    #@@song_count += 1
+    @@song_count += 1
   end
 
   def self.song_count
