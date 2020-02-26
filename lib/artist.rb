3f05d3997ffdffdc172f598d417180842364a404
Dakota States <dakota.states@gmail.com>
2020-02-26 06:10:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,17 +16,17 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    self.song_count += 1
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    self.song_count += 1
+    @@song_count += 1
   end
 
-  def song_count
+  def self.song_count
     @@song_count
   end
 
